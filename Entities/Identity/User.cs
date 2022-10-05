6b7a5b13dcb8aea864210fbaee2a0ae65251d2c6
InioluwaJohansson <inioluwa.makinde10@gmail.com>
2022-10-05 13:47:16 +0100

    Identity

--- Entities/Identity/User.cs
+++ Entities/Identity/User.cs
@@ -6,5 +6,5 @@ public class User{
     public string Email {get; set;}  
     public string PhoneNumber { get; set; }
     public string Password { get; set; }
-    public List<UserRole> UserRoles {get; set;} = new List<UserRoles>();
+    public List<UserRole> UserRoles {get; set;} = new List<UserRole>();
 }
--- Entities/Identity/UserRole.cs
+++ Entities/Identity/UserRole.cs
@@ -1,2 +1,9 @@
+using AuctionApp.Contracts;
 namespace AuctionApp.Entities.Identity;
-public class UserRole:
+public class UserRole: AuditableEntity
+{
+    public int UserId {get; set;}
+    public User User {get; set;}
+    public int RoleId {get; set;}
+    public Role Role {get; set;}
+}
