351ca7b7639e07ef6d13110918dd2fe600d37d37
ZafrullahId <zafrullahdaprince@gmail.com>
2022-10-05 16:08:16 +0100

    Dto updated

---
+++ DTOs/AuctionDto.cs
@@ -0,0 +1,12 @@
+using AuctionApp.Enum;
+
+namespace Dtos.AuctionDto
+{
+    public class AuctionDto
+    {
+        public decimal StartingPrice { get; set; }
+        public string AssetName { get; set; }
+        public DateTime ExpiryDate { get;set; }
+        public AuctionType AuctionType { get; set; }
+    }
+}
