c160dbfdf5e5cdaed148af1b2108bc49120e23bd
ZafrullahId <zafrullahdaprince@gmail.com>
2022-10-05 14:49:26 +0100

    Customer Added to Customer User Entity

--- Entities/Identity/User.cs
+++ Entities/Identity/User.cs
@@ -9,5 +9,6 @@ public class User : BaseEntity
     public string Email {get; set;}  
     public string PhoneNumber { get; set; }
     public string Password { get; set; }
+    public Customer Customer {get;set;}
     public List<UserRole> UserRoles {get; set;} = new List<UserRole>();
 }
