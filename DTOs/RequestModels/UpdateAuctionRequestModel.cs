66587087eea1e4378501df9e5c5a2be9b974b740
Brogramerr <fagbolade006@gmail.com>
2022-10-06 14:42:29 +0100

    update

--- DTOs/RequestModels/UpdateAuctionRequestModel.cs
+++ DTOs/RequestModels/UpdateAuctionRequestModel.cs
@@ -3,13 +3,13 @@
 namespace AuctionApplication.DTOs.RequestModels
 {
 
-        public class UpdateAuctionRequestModels
-        {
-            public int Id { get; set; }
-            public decimal StartingPrice { get; set; }
-            public string AssetName { get; set; }
-            public DateTime ExpiryDate { get; set; }
-            public AuctionType AuctionType { get; set; }
-        }
-    
+    public class UpdateAuctionRequestModels
+    {
+        public int Id { get; set; }
+        public decimal StartingPrice { get; set; }
+        public string AssetName { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public AuctionType AuctionType { get; set; }
+    }
+
 }
