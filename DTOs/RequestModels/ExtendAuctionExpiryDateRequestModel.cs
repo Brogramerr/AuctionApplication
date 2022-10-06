31cbe0a4866a134b94877c6e8af25eb7225d4cfa
Kehinde Makinde <kehinde.makinde07@gmail.com>
2022-10-06 15:05:43 +0100

    final

---
+++ DTOs/RequestModels/ExtendAuctionExpiryDateRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuctionApp.DTOs.RequestModels
+{
+    public class ExtendAuctionExpiryDateRequestModel
+    {
+        
+        public string AuctionId { get; set; }
+        public DateTime AuctionExpiryDate { get; set; }
+    }
+}
