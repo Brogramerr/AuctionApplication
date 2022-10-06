b384decd37840a0eb20a9551e94857dbccd532f1
ZafrullahId <zafrullahdaprince@gmail.com>
2022-10-06 10:02:39 +0100

    User Dto

--- DTOs/RequestModels/AuctionRequestModels.cs
+++ DTOs/RequestModels/AuctionRequestModels.cs
@@ -5,7 +5,7 @@ namespace AuctionApplication.DTOs.RequestModels
         public class CreateAuctionRequestModels
         {
             public decimal StartingPrice { get; set; }
-            public string AssetName { get; set; } 
+            public string AssetName { get; set; }
             public DateTime ExpiryDate { get; set; }
         }
     }
