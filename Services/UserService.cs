b5dfac5641d969f0fdf4f7e59aa6d4a5085565d2
ThatBrat <azeemahhamzat@student.oauife.edu.ng>
2022-10-10 11:53:02 +0100

    ServicesFolder

---
+++ Services/UserService.cs
@@ -0,0 +1,37 @@
+using AuctionApplication.Context;
+using AuctionApplication.Entities;
+using AuctionApplication.Entities.Identity;
+using AuctionApplication.Implementations.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionApplication.Services
+{
+    public class UserService
+
+    {
+        private readonly UserRepository _repository;
+        public UserService(UserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public string Login(string email, string passWord)
+        {
+
+            var user = _repository.ExistsByEmailAsync(email, passWord);
+            if (user = !null)
+            {
+                return new BaseResponse
+                {
+                    Success = true,
+                    Message = "Sucessfully logged in",
+                };
+            }
+            return new BaseResponse
+            {
+                Success = false,
+                Message = "Loggin Failed",
+            };
+        }
+    }
+}
