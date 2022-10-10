f215c7d61661779a53f9f8c257a45907c54b0caa
ThatBrat <azeemahhamzat@student.oauife.edu.ng>
2022-10-10 12:18:52 +0100

    NamespaceCorrection

--- Interface/Repositories/IAuctionrRepository.cs
+++ Interface/Repositories/IAuctionrRepository.cs
@@ -1,14 +1,10 @@
 using AuctionApplication.Entities;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-namespace AuctionApplication.Interface.Repositories;
-public interface IAuctionRepository : IGenericRepository<Auction>
+namespace AuctionApplication.Interface.Repositories
 {
+    public interface IAuctionRepository : IGenericRepository<Auction>
+    {
         Task<Auction> GetAsync(int id);
-        
-        
-        
-
-        
-       
+    }
 }
