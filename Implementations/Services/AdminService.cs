5d166871619e51672bb1b7e70490efa151e5ebbb
Brogramerr <fagbolade006@gmail.com>
2022-10-10 15:09:39 +0100

    AdminService

---
+++ Implementations/Services/AdminService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionApp.Interface.Services;
+
+namespace AuctionApp.Implementations.Services
+{
+    public class AdminService : IAdminService
+    {
+        
+    }
+}
