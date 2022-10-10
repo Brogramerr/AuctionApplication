7f86691ab9b22acbab81168ad1f3a9b3afab3523
ZafrullahId <zafrullahdaprince@gmail.com>
2022-10-10 14:18:40 +0100

    Changed CustomerDto and ICustomerRepository inherited from IGenericRepository

--- DTOs/CustomerDto.cs
+++ DTOs/CustomerDto.cs
@@ -2,6 +2,7 @@ namespace AuctionApplication.DTOs
 {
     public class CustomerDto
     {
+        public UserDto UserDto { get; set; }
         public int Id { get; set; }
         public int UserId { get; set; }
     }
