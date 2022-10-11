927bd3cd592d10f9d628d8fcc3f4c763747ecbb0
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2022-10-11 18:30:59 +0100

    update

--- Implementations/Repositories/CustomerRepository.cs
+++ Implementations/Repositories/CustomerRepository.cs
@@ -12,10 +12,42 @@ namespace AuctionApplication.Implementations.Repositories
         {
             _Context = Context;
         }
+<<<<<<< HEAD
         public async Task<Customer> GetById(int id)
         {
             var customer = await _Context.Customers.Include(c => c.User).FirstOrDefaultAsync(c => c.Id == id);
             return customer;
+=======
+
+        public async Task<Customer> AddAssetsForAuctionAsync(int id, int auctionId)
+        {
+            var customer = await _Context.Customers.Include(c => c.Assests).ThenInclude(a => a.Auction).FirstOrDefaultAsync(c => c.Id == id && c.Assests.Any(a => a.AuctionId == auctionId));
+        }
+
+        public async Task<Customer> ChangeAuctionAsync(int id, int auctionId)
+        {
+            var customer = await _Context.Customers.Include(c => c.Assests).ThenInclude(a => a.Auction).FirstOrDefaultAsync(c => c.Id == id && c.Assests.Any(a => a.AuctionId == auctionId));
+        }
+        
+        public async Task<Customer> ExistsByEmailAsync(string Email, string password)
+        {
+            var customer = await _Context.Customers.Include(c => c.User).FirstOrDefaultAsync(c => c.User.Email == Email && c.User.Password == password);
+        }
+
+        public async Task<Customer> GetAllBiddersAsync(int id)
+        {
+            var customer = await _Context.Customers.Include(x => x.Biddings).FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Customer> GetAssetsByDateTimeNowAsync(int id)
+        {
+            var customer = await _Context.Customers.Include(c => c.Assests).ThenInclude(a => a.Auction).FirstOrDefaultAsync(c => c.Id == id && c.Assests.Any(a => a.Auction.StartDate <= DateTime.Now && a.Auction.EndDate >= DateTime.Now));
+        }
+
+        public async Task<Customer> ChangeAssetsPriceAsync(int id, decimal price)
+        {
+            var customer = await _Context.Customers.Include(c => c.Assets).FirstOrDefaultAsync(c => c.Id == id && c.Assets.Price != price);
+>>>>>>> 0ea5be8 (update)
         }
         public async Task<Customer> ExistsByEmailAsync(string Email, string passWord)
         {
